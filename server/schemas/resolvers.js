b2f1e0b46114c617d5346d86596c0953f6469559
Dan Lyons <danielclyons@yahoo.com>
2021-04-13 21:37:59 -0400

    began working on adding user to database for trucking and dock

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,10 +5,48 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
 
     Query: {
-
+        truckingUser: async (parent, args, context) => {
+            if (context.user) {
+              const truckingUser = await Trucking_User.findById(context.user._id).populate({
+                // path: 'orders.products',
+                // populate: 'category'
+              });
+      
+            //   truckingUser.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+      
+              return truckingUser;
+            }
+      
+            throw new AuthenticationError('Not logged in');
+          },
+          dockUser: async (parent, args, context) => {
+            if (context.user) {
+              const dockUser = await Dock_User.findById(context.user._id).populate({
+                // path: 'orders.products',
+                // populate: 'category'
+              });
+      
+            //   truckingUser.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+      
+              return dockUser;
+            }
+      
+            throw new AuthenticationError('Not logged in');
+          },
     },
     Mutation: {
-
+        addTruckingUser: async (parent, args) => {
+            const truckingUser = await Trucking_User.create(args);
+            const token = signToken(truckingUser);
+      
+            return { token, truckingUser };
+          },
+          addDockUser: async (parent, args) => {
+            const dockUser = await Dock_User.create(args);
+            const token = signToken(dockUser);
+      
+            return { token, dockUser };
+          },
     }
 
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,6 +2,15 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
+type Auth {
+    token: ID
+    user: User
+}
+
+type Mutation {
+addTruckingUser(userName: String!, password: String!, loads: [ID]): Auth
+addDockUser(name: String!, streetAddress: String!, state: String!,  zipcode: String!, rating: Float, password: String!, loads: [ID] ): Auth
+}
 `;
 
 module.exports = typeDefs
