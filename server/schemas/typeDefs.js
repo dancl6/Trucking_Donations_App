2be8aff9feb8b280400b4d57f1f9f77b4d2658d0
Dan Lyons <danielclyons@yahoo.com>
2021-04-15 23:45:52 -0400

    worked on typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,9 +7,44 @@ type Auth {
     user: User
 }
 
+type Load {
+    _id: ID
+    streetAddress: String
+    state: String
+    zipcode: String
+    donationItem: String
+    comments: String
+    trucker: Trucking_User
+    currentStatus: String
+    dock: Dock_User
+    rating: Number
+}
+
+type Trucking_User {
+    _id: ID
+    userName: String
+    phoneNumber: String
+    loads: [Load]
+    phoneNumber: String
+}
+
+type Dock_User {
+    name: String
+    streetAddress: String
+    state: String
+    zipcode: String
+    rating: Number
+    loads: [Load]
+    phoneNumber: String
+}
+
+type Query {
+    truckingUser: Trucking_User
+}
+
 type Mutation {
-addTruckingUser(userName: String!, password: String!, loads: [ID], phoneNumber: String): Auth
-addDockUser(name: String!, streetAddress: String!, state: String!,  zipcode: String!, rating: Float, password: String!, loads: [ID], phoneNumber: String ): Auth
+addTruckingUser(userName: String!, password: String!,  phoneNumber: String): Auth
+addDockUser(name: String!, streetAddress: String!, state: String!,  zipcode: String!, rating: Float, password: String!,  phoneNumber: String ): Auth
 }
 `;
 
