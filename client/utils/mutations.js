3bd22cdb6d3164ee28bd8c70046ede6ae067f9d0
Dan Lyons <danielclyons@yahoo.com>
2021-04-18 23:56:16 -0400

    got trucking user post and dock user post to work in playground

--- client/utils/mutations.js
+++ client/utils/mutations.js
@@ -4,9 +4,7 @@ export const ADD_DOCK_USER = gql`
   mutation addDockUser($name: String!, $streetAddress: String!, $state: String!, zipcode: String!, rating: Float, $password: String!, $phoneNumber: String) {
     addDockUser(name: $name, streetAddress: $streetAddress, state: $state, zipcode: $zipcode, rating: $rating, password: $password, phoneNumber: $phoneNumber) {
       token
-      dockUser {
-        _id
-      }
+
     }
   }
 `;
@@ -15,9 +13,7 @@ export const ADD_TRUCKING_USER = gql`
   mutation addTruckingUser($userName: String!, $password: String!, $phoneNumber: String! ) {
       addTruckingUser(userName: $userName, password: $password, phoneNumber: $phoneNumber) {
       token
-      truckingUser {
-          _id
-      }
+
   }
 }
 `;
