96593e2afca5b08e332ea3bccf04946f58fa8ed5
Dan Lyons <danielclyons@yahoo.com>
2021-07-10 22:05:58 -0400

    working on using register to parseFloat

--- client/src/pages/Add_Load.js
+++ client/src/pages/Add_Load.js
@@ -4,16 +4,21 @@ import { useMutation } from '@apollo/react-hooks';
 // import Auth from "../utils/auth";
 import { ADD_LOAD } from "../utils/mutations";
 import { useForm } from "react-hook-form"
+// import { useAsyncTask } from 'react-hooks-async'
 
 // function Trucker_Signup() {
 const Load_Added = () => {
   const [formState, setFormState] = useState({streetAddress: '', state: '' , zipcode: '' , donationItem: '', number: '' , trucker: '', currentStatus: '',comments:'', dock: '', rating: '' });
   const [addLoad, { error }] = useMutation(ADD_LOAD);
-  const { register, handleSubmit, formState: { errors },
+  const { register, handleSubmit, errors
 } = useForm();
   // const cancelForm = async event => {
   //   document.getElementById("trucker").reset();
   // }
+
+  const onSubmit = data => {
+    console.log(data)
+  }
 
   const handleFormSubmit = async event => {
     event.preventDefault();
@@ -33,6 +38,7 @@ const Load_Added = () => {
     console.log(error)
   }
   
+  
   const handleChange = event => {
     const { name, value } = event.target;
     setFormState({
@@ -49,7 +55,17 @@ const Load_Added = () => {
       </Link>
 
       <h2>Add Load</h2>
-      <form onSubmit={handleFormSubmit}>
+      {/* <form onSubmit=   {handleSubmit(onSubmit)}> */}
+      <form onSubmit = {handleSubmit(async(formState) => {
+          const response =        await addLoad({
+            variables: { ...formState }
+            // variables: {
+            //   streetAddress: formState.streetAddress, state: formState.state, zipcode: formState.zipcode, donationItem: formState.donationItem, number: formState.number, trucker: formState.trucker, currentStatus: formState.currentStatus, dock: formState.dock
+            // }
+          });
+          const data = await response.json();
+          console.log(data, "server data")
+        })}>
         <div className="flex-row space-between my-2">
           <label htmlFor="streetAddress">Street Address:</label>
           <input
@@ -57,7 +73,7 @@ const Load_Added = () => {
             name="streetAddress"
             type="streetAddress"
             id="streetAddress"
-            value={formState.streetAddress}
+            // value={formState.streetAddress}
             onChange={handleChange}
           />
         </div>
@@ -68,7 +84,7 @@ const Load_Added = () => {
             name="state"
             type="state"
             id="state"
-            value={formState.state}
+            // value={formState.state}
             onChange={handleChange}
           />
         </div>
@@ -79,7 +95,7 @@ const Load_Added = () => {
             name="zipcode"
             type="zipcode"
             id="zipcode"
-            value={formState.zipcode}
+            // value={formState.zipcode}
             onChange={handleChange}
           />
         </div>
@@ -90,16 +106,16 @@ const Load_Added = () => {
             name="donationItem"
             type="donationItem"
             id="donationItem"
-            value={formState.donationItem}
+            // value={formState.donationItem}
             onChange={handleChange}
           />
         </div>
         <div className="flex-row space-between my-2">
-          <label htmlFor="test">Number:</label>
-          <input onSubmit={handleSubmit}
+          <label htmlFor="number">Number:</label>
+          <input
           type = "number" 
           {...register(
-                  "test",
+                  "number",
                   {       
                     setValueAs: v => parseInt(v)       
        
@@ -125,7 +141,7 @@ const Load_Added = () => {
             name="trucker"
             type="trucker"
             id="trucker"
-            value={formState.trucker}
+            // value={formState.trucker}
             onChange={handleChange}
           />
         </div>
@@ -136,7 +152,7 @@ const Load_Added = () => {
             name="currentStatus"
             type="currentStatus"
             id="currentStatus"
-            value={formState.currentStatus}
+            // value={formState.currentStatus}
             onChange={handleChange}
           />
         </div>
@@ -148,23 +164,31 @@ const Load_Added = () => {
             name="dock"
             type="dock"
             id="dock"
-            value={formState.dock}
+            // value={formState.dock}
             onChange={handleChange}
           />
         </div>
 
         <div className="flex-row space-between my-2">
-          <label htmlFor="rating">Rating:</label>
-          <input {...register(
+        <label htmlFor="rating">Rating:</label>
+          <input 
+          type = "rating" 
+          {...register(
                   "rating",
-            {       
-              transformValue: (value => parseFloat(value)       
- 
-          )} )}
-            placeholder="rating"
-            name="rating"
-            type="number"
-            id="rating"
+                  {       
+                    setValueAs: v => parseFloat(v)       
+       
+                  })} 
+            // placeholder="number"
+            // name="number"
+            // type="number"
+            // id="number"
+            // ref={
+            //   register({
+            //     validate: v =>
+            //     typeof v !== "number" ? "Invalid amount" : v % 2 !==0 ? "Only even amounts are allowed!" : undefined,
+            //   })
+            // }
 
             onChange={handleChange}
           />
