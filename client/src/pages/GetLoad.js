8642c2d96d6f3282dec46eaed5f8a0e5467aa30a
Dan Lyons <danielclyons@yahoo.com>
2021-10-09 23:13:23 -0400

    working on myloads page

---
+++ client/src/pages/GetLoad.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useQuery } from '@apollo/react-hooks';
+import Auth from "../utils/auth";
+import { GET_TRUCKER_LOADS, GET_LOAD } from "../utils/queries";
+import { useStoreContext } from '../utils/GlobalState'
+import { TRUCKER_LOADS } from "../utils/actions";
+
+function GetLoad({loadId}) {
+    const { loading, error, data } = useQuery(GET_LOAD, {
+        variables: {loadId},
+    })
+}
+
+export default GetLoad;
--- client/src/pages/My_Loads.js
+++ client/src/pages/My_Loads.js
@@ -5,25 +5,31 @@ import Auth from "../utils/auth";
 import { GET_TRUCKER_LOADS, GET_LOAD } from "../utils/queries";
 import { useStoreContext } from '../utils/GlobalState'
 import { TRUCKER_LOADS } from "../utils/actions";
-
+import {GetLoad} from "./GetLoad"
 
 const My_Loads = () => {
     const {loading, data} = useQuery(GET_TRUCKER_LOADS);
     const [state, dispatch] = useStoreContext();
     const { Trucker_Loads } = state;
-    const {loading: loading2,  data2 } = useQuery(GET_LOAD)
-    const getLoadInfo = async (loadId)  => {
+//     function getLoad ({loadId}) {
+//     const {loading: loading2, error, data:data2 } = useQuery(GET_LOAD, {
+//         variables: {loadId},
+//     })
+// }
+
+    
+    // const getLoadInfo = async (loadId)  => {
  
-            // data2 ({
-            //     variables: {loadId}
-            // })
+    //         // data2 ({
+    //         //     variables: {loadId}
+    //         // })
 
         
-            await data2 ({
-                variables: {loadId}
-            })
-        console.log("data 2 is:", data2)
-    }
+    //         await data2 ({
+    //             variables: {loadId}
+    //         })
+    //     console.log("data 2 is:", data2)
+    // }
 // dispatch({
 //     type: TRUCKER_LOADS,
 //     Trucker_Loads: data?.getTruckerLoads
@@ -51,7 +57,7 @@ return (
     <div>
         {data?.getTruckerLoads.map(load => (
             <div>
-                {getLoadInfo(load._id) ? (<span>{data2}</span>) : ''       }
+                {GetLoad(load._id) ? (<span>{GetLoad(load._id)}</span>) : ''       }
             
            <p>
                {/* <span>State: {data2.state}</span>                */}
